990f70a4feac6a2faaeee86eba95299064c35402
Toby Mahnert <toby.mahnert@rocketboots.com>
2021-12-15 22:17:09 +1100

    Added first app and created simple view

---
+++ firstProject/firstApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from firstApp.models import Employee
+
+
+# Create your views here.
+def employeeView(request):
+    data = Employee.objects.all()
+    response = {'employees': list(data.values('name', 'sal'))}
+    return JsonResponse(response)
--- firstProject/firstProject/settings.py
+++ firstProject/firstProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'firstApp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'firstProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'employeedb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
