0679d7718f0efe0e02025043e62b692828919ade
Toby Mahnert <toby.mahnert@rocketboots.com>
2021-12-17 19:29:45 +1100

    practice generic based views

--- cbvSerialisers/courseApp/views.py
+++ cbvSerialisers/courseApp/views.py
@@ -3,36 +3,45 @@ from courseApp.serializers import CourseSerializer
 from rest_framework import generics, mixins
 
 
-class CourseList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView
-):
+class CourseList(generics.ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-    def get(self, request):
-        return self.list(request)
 
-    def post(self, request):
-        return self.create(request)
-
-
-class CourseDetail(
-    mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin, generics.GenericAPIView
-):
+class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-    def get(self, request, pk):
-        return self.retrieve(request, pk)
-
-    def put(self, request, pk):
-        return self.update(request, pk)
-
-    def delete(self, request, pk):
-        return self.destroy(request, pk)
+# class CourseList(
+#     mixins.ListModelMixin,
+#     mixins.CreateModelMixin,
+#     generics.GenericAPIView
+# ):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializer
+#
+#     def get(self, request):
+#         return self.list(request)
+#
+#     def post(self, request):
+#         return self.create(request)
+#
+#
+# class CourseDetail(
+#     mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+#     mixins.DestroyModelMixin, generics.GenericAPIView
+# ):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializer
+#
+#     def get(self, request, pk):
+#         return self.retrieve(request, pk)
+#
+#     def put(self, request, pk):
+#         return self.update(request, pk)
+#
+#     def delete(self, request, pk):
+#         return self.destroy(request, pk)
 #
 #
 # class CourseList(APIView):
