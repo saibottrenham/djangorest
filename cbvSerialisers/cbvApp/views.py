70cc6e234b1d9916c256aa3b080801831d5043fc
Toby Mahnert <toby.mahnert@rocketboots.com>
2021-12-17 19:27:45 +1100

    exploring generics based views

--- cbvSerialisers/cbvApp/views.py
+++ cbvSerialisers/cbvApp/views.py
@@ -1,43 +1,48 @@
-from django.shortcuts import render
 from cbvApp.models import Student
 from cbvApp.serializers import StudentSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from django.http import Http404
-from rest_framework import generics, mixins
+from rest_framework import generics
 
 
-class StudentList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView
-):
+class StudentList(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self, request):
-        return self.list(request)
 
-    def post(self, request):
-        return self.create(request)
-
-
-class StudentDetail(
-    mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin, generics.GenericAPIView
-):
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self, request, pk):
-        return self.retrieve(request, pk)
 
-    def put(self, request, pk):
-        return self.update(request, pk)
-
-    def delete(self, request, pk):
-        return self.destroy(request, pk)
+# class StudentList(
+#     mixins.ListModelMixin,
+#     mixins.CreateModelMixin,
+#     generics.GenericAPIView
+# ):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#
+#     def get(self, request):
+#         return self.list(request)
+#
+#     def post(self, request):
+#         return self.create(request)
+#
+#
+# class StudentDetail(
+#     mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+#     mixins.DestroyModelMixin, generics.GenericAPIView
+# ):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#
+#     def get(self, request, pk):
+#         return self.retrieve(request, pk)
+#
+#     def put(self, request, pk):
+#         return self.update(request, pk)
+#
+#     def delete(self, request, pk):
+#         return self.destroy(request, pk)
 
 #
 # class StudentList(APIView):
