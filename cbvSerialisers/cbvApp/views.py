8c03fdc96af621e58ea9fc1ec34411ff88e0af49
Toby Mahnert <toby.mahnert@rocketboots.com>
2021-12-17 22:36:33 +1100

    exploring pagination

--- cbvSerialisers/cbvApp/views.py
+++ cbvSerialisers/cbvApp/views.py
@@ -1,11 +1,17 @@
 from cbvApp.models import Student
 from cbvApp.serializers import StudentSerializer
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+
+
+class StudentPagination(PageNumberPagination):
+    page_size = 2
 
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    pagination_class = LimitOffsetPagination
 
 
 # class StudentList(generics.ListCreateAPIView):
