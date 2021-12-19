4101a52b6dc4ba3f383a8622a8cb1ee49048e868
Toby Mahnert <toby.mahnert@rocketboots.com>
2021-12-19 14:28:19 +1100

    playing around with combinations of view sets and function methods

---
+++ flightServices/flightApp/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Flight(models.Model):
+    flightNumber = models.CharField(max_length=10)
+    operatingAirlines = models.CharField(max_length=20)
+    departureCity = models.CharField(max_length=20)
+    arrivalCity = models.CharField(max_length=20)
+    dateOfDeparture = models.DateField(max_length=20)
+    estimatedTimeOfDeparture = models.TimeField(max_length=20)
+
+
+class Passenger(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    middleName = models.CharField(max_length=20)
+    email = models.EmailField(max_length=20)
+    phone = models.CharField(max_length=10)
+
+
+class Reservation(models.Model):
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
---
+++ flightServices/flightApp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from flightApp.models import Flight, Passenger, Reservation
+from flightApp.serializers import FlightSerializer, PassengerSerializer, ReservationSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+
+@api_view(['POST'])
+def find_flights(request):
+    flights = Flight.objects.filter(
+        departureCity=request.data['departureCity'],
+        arrivalCity=request.data['arrivalCity'],
+        dateOfDeparture=request.data['dateOfDeparture'],
+    )
+    serializer = FlightSerializer(flights, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def save_reservation(request):
+    Reservation.objects.create(
+        flight=Flight.objects.get(id=request.data['flightId']),
+        passenger=Passenger.objects.create(
+            firstName=request.data['firstName'],
+            lastName=request.data['lastName'],
+            middleName=request.data['middleName'],
+            email=request.data['email'],
+            phone=request.data['phone'],
+        ),
+    )
+    return Response(status=status.HTTP_201_CREATED)
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    queryset = Passenger.objects.all()
+    serializer_class = PassengerSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
