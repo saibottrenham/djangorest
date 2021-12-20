6a51b03bd7684dcb1a091d8fb7e3fdff090be001
Toby Mahnert <toby.mahnert@rocketboots.com>
2021-12-20 16:32:32 +1100

    added a angular app to understand api fe internaction dynamic

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FindFlightsComponent } from './components/find-flights/find-flights.component';
+import { DisplayFlightsComponent } from './components/display-flights/display-flights.component';
+import { PassengerDetailsComponent } from './components/passenger-details/passenger-details.component';
+import { ConfirmComponent } from './components/confirm/confirm.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FindFlightsComponent,
+    DisplayFlightsComponent,
+    PassengerDetailsComponent,
+    ConfirmComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-app/src/app/components/find-flights/find-flights.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find-flights',
+  templateUrl: './find-flights.component.html',
+  styleUrls: ['./find-flights.component.scss']
+})
+export class FindFlightsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
